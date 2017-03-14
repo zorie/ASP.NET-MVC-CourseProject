e336da28921368e17848304776da4fc4d8ccf95c
zorie <shindova@abv.bg>
2017-03-14 17:52:16 +0200

    Ninject + little more setup

---
+++ DearDiary/DearDiary.Web/App_Start/DbConfig.cs
@@ -0,0 +1,15 @@
+﻿using DearDiary.Data;
+using System.Data.Entity;
+using DearDiary.Data.Migrations;
+
+namespace DearDiary.Web.App_Start
+{
+    public class DbConfig
+    {
+        public static void Initialize()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<DearDiaryDbContext, Configuration>());
+            DearDiaryDbContext.Create().Database.Initialize(true);
+        }
+    }
+}
---
+++ DearDiary/DearDiary.Web/App_Start/NinjectModules/DataNinjectModule.cs
@@ -0,0 +1,17 @@
+﻿using DearDiary.Data;
+using DearDiary.Data.Repositories;
+using Ninject.Modules;
+using Ninject.Web.Common;
+
+namespace DearDiary.Web.App_Start.NinjectModules
+{
+    public class DataNinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            this.Bind<IDearDiaryDbContext>().To<DearDiaryDbContext>().InRequestScope();
+            this.Bind(typeof(IEFGenericRepository<>)).To(typeof(EFGenericRepository<>)).InRequestScope();
+            this.Bind<IDearDiaryData>().To<DearDiaryData>().InRequestScope();
+        }
+    }
+}
