02023b7c787c3ece047ee8c2f4b9b025dc9f45ff
zorie <shindova@abv.bg>
2017-03-17 16:24:46 +0200

    unitTests & integrationTests projects

---
+++ DearDiary/DearDiary.IntegrationTests/IntegrationTest.cs
@@ -0,0 +1,23 @@
+﻿using DearDiary.Data;
+using NUnit.Framework;
+using System.Linq;
+
+namespace DearDiary.IntegrationTests
+{
+    [TestFixture]
+    public class IntegrationTest
+    {
+        [Test]
+        public void IntegrationTestMethod()
+        {
+            // Arrange
+            DearDiaryDbContext dbContext = new DearDiaryDbContext();
+
+            // Act
+            int aimsCount = dbContext.Aims.Count();           
+
+            // Assert
+            Assert.AreEqual(0, aimsCount);
+        }
+    }
+}
---
+++ DearDiary/DearDiary.UnitTests/UnitTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace DearDiary.UnitTests
+{
+    [TestFixture]
+    public class UnitTest
+    {
+        [Test]
+        public void Test()
+        {
+            // Arrange & act & assert
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
